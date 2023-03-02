64a3551c15bf2ca33f7bdb19a4ad21ff12da9f63
Rayhan Shorker <raihanshorker56@gmail.com>
2023-03-02 22:55:45 +0600

    added all AI card

--- index.js
+++ index.js
@@ -8,13 +8,27 @@ const showAllAi=data=>{
     const aiContainer = document.getElementById('ai-container');
     data.forEach(allAi =>{
         console.log(allAi);
-        const {image}=allAi;
+        const {image,features,published_in,name        }=allAi;
         const div = document.createElement('div');
+
         div.classList.add('card-body');
         div.innerHTML=`
     <figure><img src="${image}"/></figure>
-        <h2 class="card-title"></h2>
-        <p></p>
+        <h2 class="card-title text-2xl">Feature</h2>
+        <p>1.${features[0]}</p>
+        <p>2.${features[1]}</p>
+        <p class="mb-5">3.${features[2]}</p>
+        <hr class="mb-3">
+        <div class="flex justify-between ">
+        <div> 
+         <h2 class="card-title text-2xl mb-3">${name}</h2>
+        <i class="fa-sharp fa-regular fa-calendar-days text-sm">${'  '+published_in}</i>
+        </div>
+        <div class="mt-5">
+        <i class="fa-solid fa-arrow-right"></i>
+        </div>
+        </div>
+      
       
    
         `
